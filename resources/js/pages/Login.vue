c8918cbe28abdce484abe6fd740eab73192a3272
MasanaIto <z3heng0326@gmail.com>
2020-02-14 21:13:55 +0900

    会員登録フォームを実装

--- resources/js/pages/Login.vue
+++ resources/js/pages/Login.vue
@@ -25,7 +25,21 @@
         </div>
       </form>
     </div>
-    <div class="panel" v-show="tab === 2">Register Form</div>
+    <div class="panel" v-show="tab === 2">
+      <form class="form" @submit.prevent="register">
+        <label for="username">Name</label>
+        <input type="text" class="form__item" id="username" v-model="registerForm.name">
+        <label for="email">Email</label>
+        <input type="text" class="form__item" id="email" v-model="registerForm.email">
+        <label for="password">Password</label>
+        <input type="password" class="form__item" id="password" v-model="registerForm.password">
+        <label for="password-confirmation">Password (confirm)</label>
+        <input type="password" class="form__item" id="password-confirmation" v-model="registerForm.password_confirmation">
+        <div class="form__button">
+          <button type="submit" class="button button--inverse">register</button>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -36,13 +50,22 @@ export default {
       tab: 1,
       loginForm: {
         email: '',
-        password: ''
+        password: '',
       },
+      registerForm: {
+        name: '',
+        email: '',
+        password: '',
+        password_confirmation: '',
+      }
     }
   },
   methods: {
     login () {
       console.log(this.loginForm)
+    },
+    register () {
+      console.log(this.registerForm)
     }
   }
 }
