555d526b356148b432f261de4c868933c9c06a2a
MasanaIto <z3heng0326@gmail.com>
2020-02-15 00:52:54 +0900

    会員登録機能用のストアを実装、コンポーネントを実装し接続

--- resources/js/store/auth.js
+++ resources/js/store/auth.js
@@ -1,10 +1,32 @@
-const state = {}
+import Axios from "axios"
+
+/**
+ * ログイン済みユーザーを保持する
+ */
+const state = {
+    user: null
+}
 
 const getters = {}
 
-const mutations = {}
+/**
+ * userステートの値を更新する
+ */
+const mutations = {
+    setUser(state, user) {
+        state.user = user
+    }
+}
 
-const actions = {}
+/**
+ * 会員登録APIを呼び出す
+ */
+const actions = {
+    async register(context, data) {
+        const response = await Axios.post('/api/register', data)
+        context.commit('setUser', response.data)
+    }
+}
 
 export default {
     namespaced: true,
