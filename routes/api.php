eecd12f4bfae345c771c442677e140fcacf49990
MasanaIto <z3heng0326@gmail.com>
2020-02-17 12:10:28 +0900

    ユーザー取得API実装、テスト

--- routes/api.php
+++ routes/api.php
@@ -2,11 +2,13 @@
 
 use Illuminate\Http\Request;
 
-// ????
+// 会員登録
 Route::post('/register', 'Auth\RegisterController@register')->name('register');
 
-// ????
+// ログイン
 Route::post('/login', 'Auth\LoginController@login')->name('login');
 
-// ?????
+// ログアウト
 Route::post('/logout', 'Auth\LoginController@logout')->name('logout');
+
+Route::get('/user', fn() => Auth::user())->name('user');
