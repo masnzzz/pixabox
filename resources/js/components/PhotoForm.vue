a78ed996a36d02cb99756503378651f4b6725727
MasanaIto <z3heng0326@gmail.com>
2020-02-19 01:00:02 +0900

    PhotoFormコンポーネントを実装
    ナビゲーションのSubmit a Photoクリックで表示、もう一度クリックで非表示にする

---
+++ resources/js/components/PhotoForm.vue
@@ -0,0 +1,23 @@
+<template>
+  <div v-show="value" class="photo-form">
+    <h2 class="title">Submit a photo</h2>
+    <form class="form">
+      <input class="form__item" type="file">
+      <div class="form__button">
+        <button type="submit" class="button button--inverse">submit</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        value: {
+            // 表示・非表示を真偽値で表現する為Boolean型
+            type: Boolean,
+            required: true
+        }
+    }
+}
+</script>
