99ca97bbcf83527fed9f44076339ef732ba1bbdd
MasanaIto <z3heng0326@gmail.com>
2020-02-19 22:43:53 +0900

    写真一覧表示画面フロントを実装
    Photoコンポーネント（写真表示、マウスオーバー時のオーバーレイ、いいねボタン、ダウンロードボタン、投稿者名）
    PhotoListコンポーネント

--- resources/js/pages/PhotoList.vue
+++ resources/js/pages/PhotoList.vue
@@ -1,3 +1,51 @@
 <template>
-  <h1>Photo List</h1>
+  <div class="photo-list">
+    <div class="grid">
+      <Photo
+        class="grid__item"
+        v-for="photo in photos"
+        :key="photo.id"
+        :item="photo"
+      />
+    </div>
+  </div>
 </template>
+
+<script>
+import { OK } from '../util'
+import Photo from '../components/Photo.vue'
+
+export default {
+  components: {
+    Photo
+  },
+  data() {
+    return {
+      photos: []
+    }
+  },
+  methods: {
+    async fetchPhotos() {
+      const response = await axios.get('/api/photos')
+
+      if (response.status !== OK) {
+        this.$store.commit('error/setCode', response.status)
+        return false
+      }
+
+      // response.dataでレスポンスのJSONを取得し、その中のdataから写真一覧を取得
+      this.photos = response.data.data
+    }
+  },
+  watch: {
+    // $routeを監視し、ページが切り替わったときにfetchPhotosを実行
+    $route: {
+      async handler() {
+        await this.fetchPhotos()
+      },
+      // コンポーネントが生成されたタイミングでも実行
+      immediate: true
+    }
+  }
+}
+</script>
